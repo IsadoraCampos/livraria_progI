21eab58de12ee761d8820157834f963cdcdd9c49
IsadoraCampos <149686078+IsadoraCampos@users.noreply.github.com>
2024-11-01 10:31:29 -0300

    Update Estoque.java

--- Classes/Estoque.java
+++ Classes/Estoque.java
@@ -7,12 +7,20 @@ public class Estoque {
    // private boolean disponivel; quando um livro é cadastrado, ele recebe o código de disponível (melhorar esta parte)
 
     public void cadastrarLivro(Livro livro) {
-          listaLivrosDisponiveis.add(livro);
-          livro.setDisponivel(true);
-          quantidadeLivros += 1;
+        if (!listaLivrosDisponiveis.contains(livro)) {
+           listaLivrosDisponiveis.add(livro);
+           livro.setDisponivel(true);
+           quantidadeLivros += 1;
+        } else {
+            System.out.println("O livro já está cadastrado!");
+        }
     }
 
     public int getQuantidadeLivros() {
         return quantidadeLivros;
+    }
+
+    public boolean pesquisarLivro(Livro livro) {
+        return listaLivrosDisponiveis.contains(livro); //Retorna true se o livro existe, false o contrário
     }
 }
