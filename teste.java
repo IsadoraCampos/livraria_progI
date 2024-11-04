207d1f2ba82289d55f2ae9e81b7f9647f75848e3
IsadoraCampos <149686078+IsadoraCampos@users.noreply.github.com>
2024-11-04 09:35:02 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -1,13 +1,17 @@
 public class teste {
     public static void main(String args[]) {
         Autor vitorHugo = new Autor("Vitor Hugo");
-        Livro bateAsas = new Livro("Bate Asaas", 267, 19, vitorHugo , "Editora");
+        Genero ficcao = new Ficcao("Ficção");
+        Livro bateAsas = new Livro("Bate Asaas", 267, 19, vitorHugo , "Editora", ficcao);
 
         Estoque estoque = new Estoque();
 
         vitorHugo.adicionarLivroPorAutor(bateAsas);
         estoque.cadastrarLivro(bateAsas);
+        System.out.println(bateAsas.getPreco());
+        System.out.println(estoque.pesquisarLivro(bateAsas));
         estoque.excluirLivro(bateAsas);
+        System.out.println(estoque.pesquisarLivro(bateAsas));
         System.out.println(estoque.getQuantidadeLivros());
 
         if (estoque.pesquisarLivro(bateAsas)) {
