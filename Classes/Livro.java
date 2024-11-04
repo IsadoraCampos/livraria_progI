8ff16db3ad150f8a4fcf99aebb20464f95b5122c
IsadoraCampos <149686078+IsadoraCampos@users.noreply.github.com>
2024-11-04 09:34:25 -0300

    Update Livro.java

--- Classes/Livro.java
+++ Classes/Livro.java
@@ -4,14 +4,18 @@ public class Livro {
     private int capitulos;
     private Autor autor;
     private String editora;
+    private Genero genero;
+    private double preco;
     private boolean disponivel; //Outras classes podem mudar a situação do livro
 
-    public Livro(String titulo, int paginas, int capitulos, Autor autor, String editora) {
+    public Livro(String titulo, int paginas, int capitulos, Autor autor, String editora, Genero genero) {
         this.titulo = titulo;
         this.paginas = paginas;
         this.capitulos = capitulos;
         this.autor = autor;
         this.editora = editora;
+        this.genero = genero;
+        atualizarPreco();
     }
 
     public String getTitulo() {
@@ -34,6 +38,10 @@ public class Livro {
         return editora;
     }
 
+    public Genero getGenero() {
+        return genero;
+    }
+
    public boolean isDisponivel() {
         return disponivel;
     }
@@ -42,5 +50,16 @@ public class Livro {
         this.disponivel = disponivel;
     } 
 
+    public double getPreco() {
+        return preco;
+    }
+
+    private void atualizarPreco() {
+        if (genero instanceof CalculaPrecoLivro) { // Verifica se o gênero implementa a interface
+            preco = ((CalculaPrecoLivro) genero).calculaPreco(this); //Realiza um cast para que gênero seja tratado como um CalculaPrecoLivro
+        } else {
+            preco = 0.0;
+        }
+    }
     
 }
