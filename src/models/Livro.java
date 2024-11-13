1d4188cebe9fd77820e1e0e8b0bbafc14c3ee856
luisafkroth <luisafkroth@gmail.com>
2024-11-13 10:43:32 -0300

    Atualização da classe Livro

--- src/models/Livro.java
+++ src/models/Livro.java
@@ -1,25 +1,22 @@
 package src.models;
 
-import src.service.CalculaPrecoLivro;
-
 public class Livro {
     private String titulo;
     private int paginas;
     private int capitulos;
     private Autor autor;
-    private String editora;
+    private Editora editora;
     private Genero genero;
-    private double preco;
-    private boolean disponivel; //Outras classes podem mudar a situação do livro
+    private int quantidade; //Outras classes podem mudar a situação do livro
 
-    public Livro(String titulo, int paginas, int capitulos, Autor autor, String editora, Genero genero) {
+    public Livro(String titulo, int paginas, int capitulos, Autor autor, Editora editora, Genero genero, int quantidade) {
         this.titulo = titulo;
         this.paginas = paginas;
         this.capitulos = capitulos;
         this.autor = autor;
         this.editora = editora;
         this.genero = genero;
-        atualizarPreco();
+        this.quantidade = quantidade;
     }
 
     public String getTitulo() {
@@ -38,7 +35,7 @@ public class Livro {
         return autor;
     }
 
-    public String getEditora() {
+    public Editora getEditora() {
         return editora;
     }
 
@@ -46,35 +43,37 @@ public class Livro {
         return genero;
     }
 
-    public boolean isDisponivel() {
-        return disponivel;
+    public int getQuantidade() {
+        return quantidade;
     }
-
-    public void setDisponivel(boolean disponivel) {
-        this.disponivel = disponivel;
-    } 
 
     public double getPreco() {
-        return preco;
+        return this.genero.getPrecoBase();
     }
-    private void atualizarPreco() {
-        if (genero instanceof CalculaPrecoLivro) { // Verifica se o gênero implementa a interface
-            preco = ((CalculaPrecoLivro) genero).calculaPreco(this); //Realiza um cast para que gênero seja tratado como um CalculaPrecoLivro
+
+    public void reporLivro(int quantidade) {
+        this.quantidade += quantidade;
+    }
+
+    public void venderLivro(int quantidade) {
+        if (this.quantidade < quantidade) {
+            System.out.println("Quantidade insuficiente em estoque!");
         } else {
-            preco = 0.0;
+            this.quantidade -= quantidade;
+            System.out.println("Livro(s) vendido(s) com sucesso!");
         }
     }
 
     public void printLivro() {
         System.out.println("----------------------------------------------");
         System.out.println("Título: " + this.getTitulo());
-        System.out.println("Autor: " + this.autor.getNome());
-        System.out.println("Gênero: " + this.genero.getGenero());
-        System.out.println("Editora: " + this.getEditora());
+        System.out.println("Autor: " + this.getAutor().getNome());
+        System.out.println("Gênero: " + this.getGenero());
+        System.out.println("Editora: " + this.getEditora().getNome());
         System.out.println("Nº de páginas: " + this.getPaginas());
         System.out.println("Nº de capítulos: " + this.getCapitulos());
         System.out.println("Preço: " + this.getPreco());
-        System.out.println("Disponível: " + this.isDisponivel());
+        System.out.println("Quantidade disponível: " + this.getQuantidade());
         System.out.println("----------------------------------------------");
    }
 }
