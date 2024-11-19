0a72ab40690ce9b143fa38ced546e0dffa6dcc3c
IsadoraCampos <149686078+IsadoraCampos@users.noreply.github.com>
2024-11-19 08:54:00 -0300

    Update Autor.java

--- src/models/Autor.java
+++ src/models/Autor.java
@@ -3,6 +3,10 @@ package src.models;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Definição da classe Autor
+ */
+
 public class Autor {
     private String nome;
     private Editora editora;
@@ -13,6 +17,10 @@ public class Autor {
         this.editora = editora;
     }
 
+    /**
+     * Método que adiciona um livro relacionado com o autor
+     * recebe um @param de um objeto Livro e guarda em um array de Livros
+     */
     public void adicionarLivroPorAutor(Livro livro) {
         if (!livros.contains(livro)) {
             livros.add(livro);
@@ -21,14 +29,23 @@ public class Autor {
         }
     }
     
+    /**
+     * Método @return o nome do Autor
+     */
     public String getNome() {
         return this.nome;
     }
 
+    /**
+     * Método @return o nome da Editora
+     */
     public Editora getEditora() {
         return this.editora;
     }
 
+    /**
+     * Método @return os Livros do Autor
+     */
     public List<Livro> getLivros() {
         return livros;
     }
