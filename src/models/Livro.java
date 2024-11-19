419138f6db416e66303a0d0d8e5f3ac5dd3dcf2f
IsadoraCampos <149686078+IsadoraCampos@users.noreply.github.com>
2024-11-19 08:56:48 -0300

    Update Livro.java

--- src/models/Livro.java
+++ src/models/Livro.java
@@ -1,5 +1,8 @@
 package src.models;
 
+/**
+ * Definição da classe Livro
+ */
 public class Livro {
     private String titulo;
     private int paginas;
@@ -9,6 +12,9 @@ public class Livro {
     private Genero genero;
     private int quantidade;
 
+    /**
+     * Método construtor
+     */
     public Livro(String titulo, int paginas, int capitulos, Autor autor, Editora editora, Genero genero, int quantidade) {
         this.titulo = titulo;
         this.paginas = paginas;
@@ -19,42 +25,82 @@ public class Livro {
         this.quantidade = quantidade;
     }
 
+    /**
+     * Método @return o título do Livro
+     */
     public String getTitulo() {
         return titulo;
     }
 
+    
+    /**
+     * Método @return o número de páginas do Livro
+     */
     public int getPaginas() {
         return paginas;
     }
 
+    
+    /**
+     * Método @return o número de capítulos do Livro
+     */
     public int getCapitulos() {
         return capitulos;
     }
 
+    
+    /**
+     * Método @return o Autor do Livro
+     */
     public Autor getAutor() {
         return autor;
     }
 
+    
+    /**
+     * Método @return a Editora do Livro
+     */
     public Editora getEditora() {
         return editora;
     }
 
+    
+    /**
+     * Método @return o Genero do Livro
+     */
     public Genero getGenero() {
         return genero;
     }
 
+    
+    /**
+     * Método @return a quantidade do Livro cadastrada no Estoque
+     */
     public int getQuantidade() {
         return quantidade;
     }
 
+    
+    /**
+     * Método @return o preço do Livro
+     */
     public double getPreco() {
         return this.genero.getPrecoBase();
     }
 
+    
+    /**
+     * Método que repõem um Livro
+     * recebe como @param a quantidade que deseja repor e aumenta a @param quantidade do Livro
+     */
     public void reporLivro(int quantidade) {
         this.quantidade += quantidade;
     }
 
+    /**
+     * Método que vende um Livro
+     * recebe como @param a quantidade que deseja comprar e diminui a @param qauntidade do Livro
+     */
     public void venderLivro(int quantidade) {
         if (this.quantidade < quantidade) {
             System.out.println("Quantidade insuficiente em estoque!");
@@ -64,6 +110,9 @@ public class Livro {
         }
     }
 
+    /**
+     * Método que mostra as informações do Livro
+     */
     public void printLivro() {
         System.out.println("----------------------------------------------");
         System.out.println("Título: " + this.getTitulo());
