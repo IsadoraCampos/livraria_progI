47bfe70852be7e16b22cdcda3c0c2ac337bcc87a
IsadoraCampos <149686078+IsadoraCampos@users.noreply.github.com>
2024-11-19 08:58:19 -0300

    Update NumCheck.java

--- src/utilities/NumCheck.java
+++ src/utilities/NumCheck.java
@@ -1,7 +1,15 @@
 package src.utilities;
 
+/**
+ * Definição da classe NumCheck
+ */
 public class NumCheck {
 
+    /**
+     * Método que verifica se um número é inteiro
+     * recebe como @param input
+     * e @return um valor booleano
+     */
     public static boolean isInt(String input) {
         try {
             Integer.parseInt(input);
