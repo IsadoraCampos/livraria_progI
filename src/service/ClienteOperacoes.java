f3ff8fcf4c0394f48725427722b687fbd6151a2d
IsadoraCampos <149686078+IsadoraCampos@users.noreply.github.com>
2024-11-19 08:57:34 -0300

    Update ClienteOperacoes.java

--- src/service/ClienteOperacoes.java
+++ src/service/ClienteOperacoes.java
@@ -2,9 +2,20 @@ package src.service;
 
 import src.models.Livro;
 
+/**
+ * Definição da interface ClienteOperacoes
+ */
 public interface ClienteOperacoes {
 
+    /**
+     * Método que mostra as informações do Cliente
+     */
     public void printCliente();
 
+    /**
+     * Método que calcula o preço do Livro
+     * recebe como @param livro
+     * e @return o preço do Livro
+     */
     public double calculaPreco(Livro livro);
 }
