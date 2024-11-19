3e891051a75882d0aff5ddc541d37ee069513ea4
IsadoraCampos <149686078+IsadoraCampos@users.noreply.github.com>
2024-11-19 08:57:13 -0300

    Update ClienteManager.java

--- src/service/ClienteManager.java
+++ src/service/ClienteManager.java
@@ -11,15 +11,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Definição da classe ClienteManager
+ */
 public class ClienteManager {
 
     private List<Cliente> listaClientes = new ArrayList<>();
 
-
+    /**
+     * Método que retorna a @param lista de clientes
+     */
     public List<Cliente> getListaClientes() {
         return listaClientes;
     }
 
+    /**
+     * Método que cadastra um Cliente
+     * recebe como @param um Cliente e o adiciona na @param lista de clientes
+     */
     public void cadastrarCliente(Cliente cliente) {
         if (!listaClientes.contains(cliente)) {
             listaClientes.add(cliente);
@@ -28,6 +37,10 @@ public class ClienteManager {
         }
     }
 
+    /**
+     * Método que busca um Cliente pelo cpf
+     * recebe como @param o cpf e @return o Cliente
+     */
     public Cliente getClientePorCPF(String cpf) {
         for (Cliente cliente : listaClientes) {
             if (Objects.equals(cliente.getCpf(), cpf)) {
@@ -37,14 +50,28 @@ public class ClienteManager {
         return null;
     }
 
+    /**
+     * Método que verifica se um Cliente existe
+     * recebe como @param cpf
+     * e @return um valor booleano
+     */
     public boolean clienteExiste(String cpf) {
         return listaClientes.contains(getClientePorCPF(cpf));
     }
 
+    /**
+     * Método que converte um ClienteNormal para ClienteVIP
+     * recebe como @param Cliente 
+     * e @return um ClienteVIP
+     */
     public ClienteVIP converterCliente(Cliente cliente) {
         return new ClienteVIP(cliente.getNome(), cliente.getCpf(), cliente.getTelefone());
     }
 
+    /**
+     * Método que exclui um Cliente
+     * recebe como @param cpf
+     */
     public void excluirCliente(String cpf) {
         for (Cliente cliente : listaClientes) {
             if (Objects.equals(cliente.getCpf(), cpf)) {
@@ -58,6 +85,11 @@ public class ClienteManager {
         }
     }
 
+    /**
+     * Método que vende um Livro
+     *  recebe como @param cliente
+     * e @param livro
+     */
     public void venderLivro(Cliente cliente, Livro livro) {
         if (cliente instanceof ClienteNormal) {
             System.out.println("CLIENTE NORMAL");
